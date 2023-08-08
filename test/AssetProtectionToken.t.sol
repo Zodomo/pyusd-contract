f5bc71f22cbf510981246e52196a87137b5c4ac9
Zodomo <zodomo@proton.me>
2023-08-07 23:05:24 -0500

    determined bug was user error, fixed issue, wrote more tests

--- test/AssetProtectionToken.t.sol
+++ test/AssetProtectionToken.t.sol
@@ -7,43 +7,108 @@ import "../src/PYUSDImplementation.sol";
 
 contract AssetProtectionTokenTest is DSTestPlus {
 
+    event AddressFrozen(address indexed addr);
+
     PYUSDImplementation public token;
+    address freezableAddress = address(1);
+    address otherAddress = address(2);
+    uint256 amount = 100;
+    uint256 approvalAmount = 40;
 
     function setUp() public {
         token = new PYUSDImplementation();
     }
 
     function _beforeProtectableTokenTests() internal {
-        hevm.prank(address(this));
-        token.setAssetProtectionRole(msg.sender);
+        // set assetProtectionRole
+        token.setAssetProtectionRole(address(this));
     }
 
+    function _beforeProtectableFreezeWhenFrozenTokenTests() internal {
+        // set assetProtectionRole
+        token.setAssetProtectionRole(address(this));
+
+        // fund contract owner with tokens
+        token.increaseSupply(amount);
+
+        // unpause token
+        token.unpause();
+
+        // give the freezableAddress some tokens
+        token.increaseSupply(amount);
+        token.transfer(freezableAddress, amount);
+
+        // approve otherAddress address to take some of those tokens from freezableAddress
+        hevm.prank(freezableAddress);
+        token.approve(otherAddress, approvalAmount);
+
+        // approve freezableAddress address to take some of those tokens from otherAddress
+        hevm.prank(otherAddress);
+        token.approve(freezableAddress, approvalAmount);
+
+        // freeze freezableAddress
+        token.freeze(freezableAddress);
+    }
+
+    // when the asset protection role is unset, reverts asset protection actions
     function testAssetProtectionActionsRoleUnsetRevert() public {
         hevm.expectRevert(PYUSDImplementation.NotAssetProtector.selector);
-        token.freeze(address(1));
+        token.freeze(freezableAddress);
         hevm.expectRevert(PYUSDImplementation.NotAssetProtector.selector);
-        token.unfreeze(address(1));
+        token.unfreeze(freezableAddress);
         hevm.expectRevert(PYUSDImplementation.NotAssetProtector.selector);
-        token.wipeFrozenAddress(address(1));
+        token.wipeFrozenAddress(freezableAddress);
     }
 
+    // as an asset protectable token, after setting the AssetProtectionRole, the current asset protection role is set
     function testProtectableTokenAssetProtectionRoleSet() public {
         _beforeProtectableTokenTests();
-        require(token.assetProtectionRole() == msg.sender, "assetProtectionRole improperly set");
+        require(token.assetProtectionRole() == address(this), "assetProtectionRole improperly set");
     }
 
+    // as an asset protectable token, freeze reverts when sender is not asset protection
     function testProtectableTokenFreezeRevertWhenNotAssetProtector() public {
         _beforeProtectableTokenTests();
-        hevm.prank(address(1));
+        hevm.prank(otherAddress);
         hevm.expectRevert(PYUSDImplementation.NotAssetProtector.selector);
-        token.freeze(address(2));
+        token.freeze(freezableAddress);
     }
 
+    // as an asset protectable token, freeze adds the frozen address
     function testProtectableTokenFreezeAddsFrozenAddress() public {
         _beforeProtectableTokenTests();
-        console2.log(msg.sender);
-        console2.log(token.assetProtectionRole());
-        token.freeze(address(1));
-        require(token.isFrozen(address(1)), "freeze didn't freeze address");
+        token.freeze(freezableAddress);
+        require(token.isFrozen(freezableAddress), "freeze didn't freeze address");
+    }
+
+    // as an asset protectable token, freeze emits an AddressFrozen event
+    function testProtectableTokenFreezeEmitsAddressFrozen() public {
+        _beforeProtectableTokenTests();
+        hevm.expectEmit(true, false, false, false);
+        emit AddressFrozen(freezableAddress);
+        token.freeze(freezableAddress);
+    }
+
+    // as an asset protectable token, freeze when frozen reverts when transfer is from frozen address
+    function testProtectableTokenWhenFrozenTransferFrozenFromRevert() public {
+        _beforeProtectableFreezeWhenFrozenTokenTests();
+        hevm.expectRevert(PYUSDImplementation.Frozen.selector);
+        hevm.prank(freezableAddress);
+        token.transfer(otherAddress, amount);
+    }
+
+    // as an asset protectable token, freeze when frozen reverts when transfer is to frozen address
+    function testProtectableTokenWhenFrozenTransferFrozenToRevert() public {
+        _beforeProtectableFreezeWhenFrozenTokenTests();
+        hevm.expectRevert(PYUSDImplementation.Frozen.selector);
+        token.transfer(freezableAddress, amount);
+    }
+
+    // as an asset protectable token, freeze when frozen reverts when transferFrom is by frozen address
+    function testProtectableTokenWhenFrozenTransferFromFrozenFromRevert() public {
+        _beforeProtectableFreezeWhenFrozenTokenTests();
+        hevm.expectRevert(PYUSDImplementation.Frozen.selector);
+        hevm.prank(freezableAddress);
+        token.transferFrom(otherAddress, otherAddress, approvalAmount);
     }
 }
